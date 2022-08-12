d9243faf601ee35c263efed1ada7e6eb54652039
Gregory Schofield <greg.c.schofield@gmail.com>
2022-08-12 10:34:50 -0400

    Print json nicer and support log levels
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -10,16 +13,42 @@ import (
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	level := os.Getenv("LOG_LEVEL")
+  if level != "" {
+  	switch strings.ToLower(level) {
+  		case "info" : {
+  			zerolog.SetGlobalLevel(zerolog.InfoLevel)
+  		}
+  		case "debug" : {
+  			zerolog.SetGlobalLevel(zerolog.DebugLevel)
+  		}
+  		case "warn" : {
+  			zerolog.SetGlobalLevel(zerolog.WarnLevel)
+  		}
+  		case "error" : {
+  			zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+  		}
+  		case "default" :{
+  			zerolog.SetGlobalLevel(zerolog.DebugLevel)
+  		}
+  	}
+  }
 	router := gin.Default()
 
 	router.HEAD("/", func(c *gin.Context) {})
 	router.POST("/webhook", func(c *gin.Context) {
 		if jsonData, err := c.GetRawData(); err != nil {
-			log.Print(err)
+			log.Error().Err(err).Msg("failed to get stream data")
+			c.String(http.StatusBadRequest, "failed to get stream data")
 		} else {
-			log.Debug().Msg(string(jsonData))
-			c.Data(http.StatusOK, gin.MIMEJSON, jsonData)
+			if !json.Valid(jsonData) {
+				log.Error().RawJSON("raw-json", jsonData).Msg("Invalid json received")
+				c.String(http.StatusBadRequest, "Invalid json received")
+			} else {
+				log.Debug().RawJSON("raw-json", jsonData).Msg("success")
+				c.Data(http.StatusOK, gin.MIMEJSON, jsonData)
+			}
 		}
 	})
 
